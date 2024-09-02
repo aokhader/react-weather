e03d2c169a5644ea0bd1f192cac7108ce63606f6
AbdulazizKhader <akhader@ucsd.edu>
2024-09-02 15:24:58 -0700

    added changes for PM Accelerator

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const button = document.getElementById('pm-button');
+button.addEventListener('click', () => {
+  alert(`The Product Manager Accelerator Program is designed to support PM professionals through every stage of their career. From students looking for entry-level jobs to Directors looking to take on a leadership role, our program has helped over hundreds of students fulfill their career aspirations. \n
+Our Product Manager Accelerator community are ambitious and committed.Through our program they have learnt, honed and developed new PM and leadership skills, giving them a strong foundation for their future endeavours.
+      `);
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
